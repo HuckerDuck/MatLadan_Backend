19132a02c11ca127afc4a01abacfee4981e25032
HuckerDuck / Fredrik <Frmb1@hotmail.com>
2025-10-23 10:29:51 +0200

    Initial commit

---
+++ src/main/java/com/fredrik/matladan/MatLadanApplication.java
@@ -0,0 +1,13 @@
+package com.fredrik.matladan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatLadanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatLadanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fredrik/matladan/MatLadanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fredrik.matladan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatLadanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
