d07289af6a859bac6d7842a0c453f5ee7ce625f9
HuckerDuck aka Fredrik <fredrikmenotbrauer@gmail.com>
2025-10-28 14:37:46 +0100

    ## Moved CorsConfig, Updated CustomUserRepository, Added Test ##
    - Moved the CorsConfig into the securityConfigfile as it makes 1 less class and there wasn't alot of code in it.
    
    - Used the wrong repository so I changed it to a JpaRepository
    
    - Added a Test for testing the server.
    
    - Need to make more text i think

--- src/main/java/com/fredrik/matladan/user/config/SecurityConfig.java
+++ src/main/java/com/fredrik/matladan/user/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.fredrik.matladan.user.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +13,17 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         //? BCrypt is usually the default way you wanna do this
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain
+            (HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        .anyRequest().permitAll()
+                );
+        return httpSecurity.build();
     }
 }
--- src/main/java/com/fredrik/matladan/user/repository/CustomUserRepository.java
+++ src/main/java/com/fredrik/matladan/user/repository/CustomUserRepository.java
@@ -1,13 +1,14 @@
 package com.fredrik.matladan.user.repository;
 
 import com.fredrik.matladan.user.model.CustomUser;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface CustomUserRepository extends CrudRepository<CustomUser, Long> {
+public interface CustomUserRepository extends JpaRepository<CustomUser, Long> {
     Optional<CustomUser> findByUsername(String username);
     Optional<CustomUser> findByEmail(String email);
 }
