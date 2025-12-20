651f893daab813f7b8fcdd0298e7bfc0237659a9
HuckerDuck aka Fredrik <fredrikmenotbrauer@gmail.com>
2025-12-20 16:10:07 +0100

    ## Enums and RecipeEntity is added ## - Added Enums for the diffrent category of MealType and for the DietType. - Added a RecipeEntity that we will use later. - This will use Flyway later and we will add the other stuff now
    Signed-off-by: HuckerDuck aka Fredrik <fredrikmenotbrauer@gmail.com>

--- src/main/java/com/fredrik/matladan/recipe/model/RecipeEntity.java
+++ src/main/java/com/fredrik/matladan/recipe/model/RecipeEntity.java
@@ -1,16 +1,18 @@
 package com.fredrik.matladan.recipe.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import com.fredrik.matladan.recipe.enums.DietType;
+import com.fredrik.matladan.recipe.enums.MealType;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @Setter
+@Table(name = "recipes")
 public class RecipeEntity {
     @Setter (AccessLevel.NONE)
     @Id
@@ -18,22 +20,44 @@ public class RecipeEntity {
     @Column(updatable = false, nullable = false)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private String description;
 
+    @Column(nullable = false)
     private String instructions;
 
+    @Column(nullable = false)
     private String imageURL;
 
+    @Column(nullable = false)
     private Integer servings;
 
+    @Column(nullable = false)
     private Integer prepTime;
 
+    @Column(nullable = false)
     private Integer cookTime;
 
+    @Enumerated(EnumType.STRING)
+    private MealType mealType;
+
+    @Enumerated(EnumType.STRING)
+    private DietType dietType;
 
 
+    @Column(name = "created_date", nullable = false)
+    private LocalDateTime createdDate;
 
+    @Column(name = "last_modified_date")
+    private LocalDateTime lastModifiedDate;
+    @PrePersist
+    void onCreatingUser(){
+        this.createdDate = LocalDateTime.now();
+        this.lastModifiedDate = this.createdDate;
+
+    }
 
 }
