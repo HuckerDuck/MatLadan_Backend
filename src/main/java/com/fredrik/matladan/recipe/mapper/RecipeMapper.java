f002a4b4cf66ac092aa14f73bb18a4fc3acd9e42
HuckerDuck aka Fredrik <fredrikmenotbrauer@gmail.com>
2025-12-29 14:17:26 +0100

    ## Adding a patch method to RecipeMapper for updating existing recipes ##
    - The patch method allows partial updates without replacing the entire entity
    - If a field in the DTO is null it will be ignored and not overwrite the existing value
    
    - ID and timestamps are still ignored since it will not be needed and I don't wanna to överwrite that
    
    Signed-off-by: HuckerDuck aka Fredrik <fredrikmenotbrauer@gmail.com>

--- src/main/java/com/fredrik/matladan/recipe/mapper/RecipeMapper.java
+++ src/main/java/com/fredrik/matladan/recipe/mapper/RecipeMapper.java
@@ -2,8 +2,7 @@ package com.fredrik.matladan.recipe.mapper;
 
 import com.fredrik.matladan.recipe.dto.CreateRecipeDTO;
 import com.fredrik.matladan.recipe.model.RecipeEntity;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface RecipeMapper {
@@ -17,4 +16,14 @@ public interface RecipeMapper {
 
     //? Entity to DTO (response)
     CreateRecipeDTO toDTO(RecipeEntity recipeEntity);
+
+    //? For the Patch method
+    //? NullValuePropertyMappingStrategy.IGNORE is used to say
+    //? If the DTO has a null value for a property, then it should be ignored
+    //? It's used for the patch method to avoid setting a null value for a field
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdDate", ignore = true)
+    @Mapping(target = "lastModifiedDate", ignore = true)
+    void patch(CreateRecipeDTO createRecipeDTO, @MappingTarget RecipeEntity recipeEntity);
 }
