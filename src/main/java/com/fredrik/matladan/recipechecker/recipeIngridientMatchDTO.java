f705e27f20d628b4d95716a45c6433baf2c6a1a9
HuckerDuck aka Fredrik <fredrikmenotbrauer@gmail.com>
2025-12-30 13:51:59 +0100

    ## First initial commit of new branch ## - This branch is to see if I can match the ingrient that we have to the recipe.
    - This will hopefully in the end check towards the database to see if we need to make the selected recipe or not
    
    Signed-off-by: HuckerDuck aka Fredrik <fredrikmenotbrauer@gmail.com>

---
+++ src/main/java/com/fredrik/matladan/recipechecker/recipeIngridientMatchDTO.java
@@ -0,0 +1,19 @@
+package com.fredrik.matladan.recipechecker;
+
+import com.fredrik.matladan.item.enums.UnitAmountType;
+
+//? Record for the response body
+//? This is used to check if the recipe is possible to make
+//? If it is possible, then the missingAmount will be 0
+//? otherwise it will be the amount that is missing
+//? boolean isAvailable is used to check if the ingredient is available
+//? this will be true or false
+public record recipeIngridientMatchDTO(
+        String ingredientName,
+        double requiredAmount,
+        UnitAmountType requiredUnit,
+        double availableAmount,
+        double missingAmount,
+        boolean isAvailable
+) {
+}
