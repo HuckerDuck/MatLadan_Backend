4439506305214353b4451f743f1a9e2b22ddabec
HuckerDuck aka Fredrik <fredrikmenotbrauer@gmail.com>
2025-12-30 20:52:50 +0100

    ## Test pass ##
    - Added a canMakeRecipe() method that get the recipe and user items.
    - Uses RecipeNotFoundException if recipe not found.
    - Gets the current user and the users items from the repo
    
    Signed-off-by: HuckerDuck aka Fredrik <fredrikmenotbrauer@gmail.com>

--- src/main/java/com/fredrik/matladan/recipechecker/service/RecipeCheckerServiceImpl.java
+++ src/main/java/com/fredrik/matladan/recipechecker/service/RecipeCheckerServiceImpl.java
@@ -1,13 +1,20 @@
 package com.fredrik.matladan.recipechecker.service;
 
+import com.fredrik.matladan.item.entity.Item;
 import com.fredrik.matladan.item.repository.ItemRepository;
+import com.fredrik.matladan.recipe.exceptions.RecipeNotFoundException;
+import com.fredrik.matladan.recipe.model.RecipeEntity;
 import com.fredrik.matladan.recipe.repository.RecipeRepository;
 
+import com.fredrik.matladan.recipechecker.dto.recipeIngridientMatchDTO;
 import com.fredrik.matladan.recipechecker.dto.recipeIngridientMatchResponseDTO;
 import com.fredrik.matladan.user.model.CustomUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,8 +25,21 @@ public class RecipeCheckerServiceImpl implements RecipeCheckerService {
 
     @Override
     public recipeIngridientMatchResponseDTO canMakeRecipe(Long recipeId) {
-        // TODO: Implement
-        return null;
+        RecipeEntity recipe = recipeRepository.findById(recipeId)
+                .orElseThrow(() -> new RecipeNotFoundException(recipeId));
+
+        CustomUser currentUser = getCurrentUser();
+        List<Item> userItems = itemRepository.findByStorageOwner(currentUser);
+        List<recipeIngridientMatchDTO> ingredientMatches = new ArrayList<>();
+
+
+        return new recipeIngridientMatchResponseDTO(
+                recipe.getId(),
+                recipe.getName(),
+                false,
+                0,
+                ingredientMatches
+        );
     }
 
     private CustomUser getCurrentUser() {
