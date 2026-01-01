1f05b3deb67f31fe86f1d2e8a93c5dce4e6602f7
HuckerDuck aka Fredrik <fredrikmenotbrauer@gmail.com>
2026-01-01 15:47:09 +0100

    ## Updated Itemcontroller, Added a Shopping List Controller ##
    - Removed some older comments from the ItemController that I saw that I won't need any more
    
    - ShoppingListController is now added followed the same principle basically as for items. So now you are able to add a manual shoppingList as well.
    
    Signed-off-by: HuckerDuck aka Fredrik <fredrikmenotbrauer@gmail.com>

---
+++ src/main/java/com/fredrik/matladan/shoppingList/controller/ShoppingListController.java
@@ -0,0 +1,60 @@
+package com.fredrik.matladan.shoppingList.controller;
+
+import com.fredrik.matladan.shoppingList.dto.CreateShoppingListDTO;
+import com.fredrik.matladan.shoppingList.dto.PatchShoppingListDTO;
+import com.fredrik.matladan.shoppingList.dto.ShoppingListResponseDTO;
+import com.fredrik.matladan.shoppingList.service.ShoppingListService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/shopping-list")
+@RestController
+public class ShoppingListController {
+
+    private final ShoppingListService shoppingListService;
+
+    //? Adding a logger to be added to this class
+    private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(ShoppingListController.class);
+
+    //? Get all items from shopping list
+    @GetMapping
+    public ResponseEntity<List<ShoppingListResponseDTO>> getAllShoppingListItems(){
+        List<ShoppingListResponseDTO> shoppingListItems = shoppingListService.getAllShoppingListItems();
+        return ResponseEntity.ok(shoppingListItems);
+    }
+
+    //? Post mapping for creating shopping list items
+    @PostMapping
+    public ResponseEntity<ShoppingListResponseDTO> addItem(
+            @Valid @RequestBody CreateShoppingListDTO createShoppingListDTO
+    ) {
+        logger.info("Adding item to shopping list: {}", createShoppingListDTO.name());
+        ShoppingListResponseDTO created = shoppingListService.createShoppingList(createShoppingListDTO);
+        return ResponseEntity.status(201).body(created);
+    }
+
+    //? Patch mapping for updating shopping list items
+    @PatchMapping("/{id}")
+    public ResponseEntity<ShoppingListResponseDTO> patchItem(
+            @PathVariable Long id,
+            @Valid @RequestBody PatchShoppingListDTO dto
+    ) {
+        logger.info("Updating shopping list item {}", id);
+        ShoppingListResponseDTO updated = shoppingListService.patchShoppingListItem(id, dto);
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteItem(@PathVariable Long id) {
+        logger.info("Deleting shopping list item {}", id);
+        shoppingListService.deleteShoppingListItem(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
